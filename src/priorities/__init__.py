6f8b048499a45811a68c65e360764b4aa815193c
ECGomes <eduardo.gomes@m-iti.org>
2025-04-14 20:31:43 +0100

    Initial implementation for a priority system based on available SoC for use and resource urgency.

--- src/priorities/__init__.py
+++ src/priorities/__init__.py
@@ -1,3 +1,4 @@
 from .base_priority import Priority
 from .type_priority import EmpiricalPriority
 from .contribution_priority import ContributionPriority
+from .weighted_priority_v0 import WeightedPriorityV0
