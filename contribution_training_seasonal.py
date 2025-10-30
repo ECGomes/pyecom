26f5973ccd1cef59928e7945402bbd0e147fd8d4
ECGomes <eduardo.gomes@m-iti.org>
2025-10-30 13:29:03 +0000

    Refactored contribution priority training for new environment

--- contribution_training_seasonal.py
+++ contribution_training_seasonal.py
@@ -8,15 +8,19 @@ import IPython.core.display_functions
 
 from src.parsers import HMParser, CotevParser
 from src.resources import Generator, Load, Storage, Aggregator, Vehicle
-from src.algorithms.rl import EnergyCommunityContributionPriorityV5
+from src.algorithms.rl import EnergyCommunityContributionPriorityV6
 
+import torch
+from ray.tune import register_env
+from ray import tune, train
+from ray.air import CheckpointConfig
+from ray.tune.schedulers import AsyncHyperBandScheduler
 from ray.rllib.algorithms.ppo import PPOConfig
 
 import warnings
-
 warnings.filterwarnings('ignore')
 
-SEASON = 'autumn'  # 'winter', 'spring', 'summer', 'autumn'
+SEASON = 'summer'  # 'winter', 'spring', 'summer', 'autumn'
 SEASON_START = 0
 SEASON_END = 0
 
@@ -31,11 +35,11 @@ data_ec.parse()
 
 # EV data from the EV4EU simulator
 data_ev = CotevParser(population_path=
-                      '/Users/ecgomes/DataspellProjects/pyecom/data/simulation_20evs_1year_15t/population_366.csv',
+                      'data/simulation_20evs_1year_15t/population_366.csv',
                       driving_history_path=
-                      '/Users/ecgomes/DataspellProjects/pyecom/data/simulation_20evs_1year_15t/ev_driving_history_366.csv',
+                      'data/simulation_20evs_1year_15t/ev_driving_history_366.csv',
                       assigned_segments_path=
-                      '/Users/ecgomes/DataspellProjects/pyecom/data/simulation_20evs_1year_15t/assigned_segments_366.csv',
+                      'data/simulation_20evs_1year_15t/assigned_segments_366.csv',
                       parse_date_start='2020',
                       parse_date_end='2020')
 data_ev.parse()
@@ -52,14 +56,14 @@ temp_costs_df = pd.DataFrame({'cost_parameter_b': data_ec.generator['cost_parame
                               'cost_ens': data_ec.load['cost_ens'][0],
                               'discharge_price': np.array(data_ec.storage['discharge_price'][2]),
                               'charge_price': np.array(data_ec.storage['charge_price'][0]),
-                              'import_contracted_p_max': data_ec.peers['import_contracted_p_max'][0, 0],
-                              'export_contracted_p_max': data_ec.peers['export_contracted_p_max'][0, 0],
+                              'import_contracted_p_max': 200,
+                              'export_contracted_p_max': 200,
                               'buy_price': data_ec.peers['buy_price'][0],
                               'sell_price': data_ec.peers['sell_price'][0]},
                              index=pd.date_range(start='2020-01-01', freq='H', periods=24))
 # Resample to 15T and forward fill the values
 temp_costs_df = temp_costs_df.resample('15T').ffill()
-print('Initial costs DataFrame:\n{}'.format(temp_costs_df))
+# print('Initial costs DataFrame:\n{}'.format(temp_costs_df))
 
 # Now fill the remaining 3 timestamps
 temp_costs_df = temp_costs_df.reindex(pd.date_range(start='2020-01-01', freq='15T', periods=96), method='ffill')
@@ -77,7 +81,7 @@ autumn_end = aux_date_range.get_loc(aux_date_range[aux_date_range.month == 11].m
 if SEASON == 'winter':
     N_STEPS = winter_end - winter_start - 1344
     SEASON_START = winter_start
-    SEASON_END = winter_end - 1344  # Remove the last two weeks for testing purposes. 14 days * 96 steps = 1344
+    SEASON_END = winter_end - 1344
 elif SEASON == 'spring':
     N_STEPS = spring_end - spring_start - 1344
     SEASON_START = spring_start
@@ -92,7 +96,7 @@ elif SEASON == 'autumn':
     SEASON_END = autumn_end - 1344
 
 for i in range(1, 21):
-    temp_data = pd.read_csv('/Users/ecgomes/Documents/PhD/housedata/Wh/H{}_Wh.csv'.format(i))
+    temp_data = pd.read_csv('data/housedata/W/H{}_W.csv'.format(i))
 
     # Fill the generator and load missing data with zeros
     temp_data = temp_data.fillna(0)
@@ -100,11 +104,11 @@ for i in range(1, 21):
     # Resample to 1H intervals
     temp_data['date'] = pd.to_datetime(temp_data['date'])
     temp_data = temp_data.set_index('date', drop=True)
-    temp_data = temp_data.resample('15T').sum()
+    temp_data = temp_data.resample('15T').mean()
 
     # Convert to kWh
-    temp_data[' Production(kWh)'] = temp_data[' Production(Wh)'] / 1000
-    temp_data[' Consumption(kWh)'] = temp_data[' Consumption(Wh)'] / 1000
+    temp_data[' Production(kW)'] = temp_data[' Production(W)'] / 1000
+    temp_data[' Consumption(kW)'] = temp_data[' Consumption(W)'] / 1000
 
     # Check if there are enough data points
     if temp_data.shape[0] < 366 * 96:
@@ -115,20 +119,20 @@ for i in range(1, 21):
     generators.append(Generator(name='ren_generator_{:02d}'.format(i),
                                 value=np.zeros(N_STEPS),
                                 lower_bound=np.zeros(N_STEPS),
-                                upper_bound=temp_data[' Production(kWh)'][SEASON_START:SEASON_END],
-                                cost=np.tile(temp_costs_df['cost_parameter_b'], (int(N_STEPS / 24))),
-                                cost_nde=np.tile(temp_costs_df['cost_nde'], (int(N_STEPS / 24))),
+                                upper_bound=temp_data[' Production(kW)'][SEASON_START:SEASON_END],
+                                cost=np.tile(temp_costs_df['cost_parameter_b'], (int(N_STEPS / 95)))[:-1],
+                                cost_nde=np.tile(temp_costs_df['cost_nde'], (int(N_STEPS / 95)))[:-1],
                                 is_renewable=True))
 
     # Create the load
     loads.append(Load(name='load_{:02d}'.format(i),
-                      value=temp_data[' Consumption(kWh)'][SEASON_START:SEASON_END],
+                      value=temp_data[' Consumption(kW)'][SEASON_START:SEASON_END],
                       lower_bound=np.zeros(N_STEPS),
-                      upper_bound=temp_data[' Consumption(kWh)'][SEASON_START:SEASON_END],
+                      upper_bound=temp_data[' Consumption(kW)'][SEASON_START:SEASON_END],
                       cost=np.ones(N_STEPS),
-                      cost_cut=np.tile(temp_costs_df['cost_cut'], (int(N_STEPS / 24))),
-                      cost_reduce=np.tile(temp_costs_df['cost_reduce'], (int(N_STEPS / 24))),
-                      cost_ens=np.tile(temp_costs_df['cost_ens'], (int(N_STEPS / 24)))))
+                      cost_cut=np.tile(temp_costs_df['cost_cut'], (int(N_STEPS / 95)))[:-1],
+                      cost_reduce=np.tile(temp_costs_df['cost_reduce'], (int(N_STEPS / 95)))[:-1],
+                      cost_ens=np.tile(temp_costs_df['cost_ens'], (int(N_STEPS / 95)))[:-1]))
 
     used_idx.append(i - 1)  # Store the index of the house used
 
@@ -136,15 +140,15 @@ for i in range(1, 21):
 storages = []
 for i in used_idx:
     storages.append(Storage(name='storage_{:02d}'.format(i + 1),
-                            value=np.tile(0.8, N_STEPS),
-                            lower_bound=np.tile(0.2, N_STEPS),
-                            upper_bound=np.tile(10, N_STEPS),
+                            value=np.tile(8.0, N_STEPS),
+                            lower_bound=np.tile(2.0, N_STEPS),
+                            upper_bound=np.tile(10.0, N_STEPS),
                             cost=np.zeros(N_STEPS),
-                            cost_discharge=np.tile(temp_costs_df['discharge_price'], (int(N_STEPS / 24))),
-                            cost_charge=np.tile(temp_costs_df['charge_price'], (int(N_STEPS / 24))),
-                            capacity_max=10,
-                            capacity_min=2,  # 0.2 * capacity_max
-                            initial_charge=0.8,
+                            cost_discharge=np.tile(temp_costs_df['discharge_price'], (int(N_STEPS / 95)))[:-1],
+                            cost_charge=np.tile(temp_costs_df['charge_price'], (int(N_STEPS / 95)))[:-1],
+                            capacity_max=10.0,
+                            capacity_min=2.0,  # 0.2 * capacity_max
+                            initial_charge=8.0,
                             discharge_efficiency=0.95,
                             discharge_max=np.tile(3.3, N_STEPS),
                             charge_efficiency=0.95,
@@ -177,28 +181,28 @@ for i in used_idx:
 aggregator = Aggregator(name='aggregator',
                         value=np.zeros(N_STEPS),
                         lower_bound=np.zeros(N_STEPS),
-                        upper_bound=np.tile(temp_costs_df['import_contracted_p_max'], (int(N_STEPS / 24))),
-                        cost=np.tile(data_ec.peers['buy_price'], (int(N_STEPS / 96))),
+                        upper_bound=np.tile(temp_costs_df['import_contracted_p_max'], (int(N_STEPS / 95)))[:-1],
+                        cost=np.tile(data_ec.peers['buy_price'], (int(N_STEPS / 95)))[:-1],
                         imports=np.zeros(N_STEPS),
                         exports=np.zeros(N_STEPS),
-                        import_cost=np.tile(temp_costs_df['buy_price'], (int(N_STEPS / 24))),
-                        export_cost=np.tile(temp_costs_df['sell_price'], (int(N_STEPS / 24))),
-                        import_max=np.tile(temp_costs_df['import_contracted_p_max'], (int(N_STEPS / 24))),
-                        export_max=np.tile(temp_costs_df['export_contracted_p_max'], (int(N_STEPS / 24))))
+                        import_cost=np.tile(temp_costs_df['buy_price'] * 2, (int(N_STEPS / 95)))[:-1],
+                        export_cost=np.tile(temp_costs_df['sell_price'], (int(N_STEPS / 95)))[:-1],
+                        import_max=np.tile(temp_costs_df['import_contracted_p_max'], (int(N_STEPS / 95)))[:-1],
+                        export_max=np.tile(temp_costs_df['export_contracted_p_max'], (int(N_STEPS / 95)))[:-1])
 
 # Create the environment and check if everything is ok
-temp_env = EnergyCommunityContributionPriorityV5(ren_generators=generators,
-                                        generators=[],
-                                        loads=loads,
-                                        storages=storages,
-                                        evs=evs,
-                                        aggregator=aggregator,
-                                        storage_penalty=1,
-                                        ev_penalty=1,
-                                        balance_penalty=1,
-                                        look_ahead=12,
-                                        max_episode_length=N_STEPS,
-                                        seed=42)
+temp_env = EnergyCommunityContributionPriorityV6(ren_generators=generators,
+                                                 generators=[],
+                                                 loads=loads,
+                                                 storages=storages,
+                                                 evs=evs,
+                                                 aggregator=aggregator,
+                                                 storage_penalty=1,
+                                                 ev_penalty=1,
+                                                 balance_penalty=1,
+                                                 look_ahead=12,
+                                                 max_episode_length=N_STEPS - 1,
+                                                 seed=42)
 temp_env.reset()
 terminations = truncations = {a: False for a in temp_env.agents}
 terminations['__all__'] = False
@@ -211,7 +215,7 @@ print('Terminated: {}'.format(terminations['__all__']))
 
 # Create the policies to train
 # The keys of the dictionary respect the class names of the agents
-gammas = {'Generator': 0.0, 'Storage': 0.9, 'Vehicle': 0.9, 'Aggregator': 0.9}
+gammas = {'Storage': 0.99, 'Vehicle': 0.99, 'Aggregator': 0.99}
 
 policies = {}
 for ev in temp_env.evs:
@@ -220,43 +224,24 @@ for ev in temp_env.evs:
                          temp_env.action_space[ev.name],
                          PPOConfig.overrides(gamma=gammas['Vehicle']))
 
-policies['storage'] = (None,
-                       temp_env.observation_space[temp_env.storages[0].name],
-                       temp_env.action_space[temp_env.storages[0].name],
-                       PPOConfig.overrides(gamma=gammas['Storage']))
+for storage in temp_env.storages:
+    policies[storage.name] = (None,
+                              temp_env.observation_space[temp_env.storages[0].name],
+                              temp_env.action_space[temp_env.storages[0].name],
+                              PPOConfig.overrides(gamma=gammas['Storage']))
 
 policies['aggregator'] = (None,
                           temp_env.observation_space['aggregator'],
                           temp_env.action_space['aggregator'],
                           PPOConfig.overrides(gamma=gammas['Aggregator']))
 
-# Create individual networks for each agent
-model_cfgs = {'ev': {'use_lstm': True,
-                     'lstm_cell_size': 256,
-                     'fcnet_hiddens': [64, 64],
-                     'fcnet_activation': 'relu',
-                     'lstm_use_prev_action': True,
-                     'lstm_use_prev_reward': True,
-                     'vf_share_layers': False},
-              'storage': {'use_lstm': True,
-                          'lstm_cell_size': 256,
-                          'fcnet_hiddens': [64, 64],
-                          'fcnet_activation': 'relu',
-                          'lstm_use_prev_action': True,
-                          'lstm_use_prev_reward': True,
-                          'vf_share_layers': False},
-              'aggregator': {'fcnet_hiddens': [1], }}
-
 # Create an RLlib Algorithm instance from a PPOConfig to learn how to
 # act in the above environment.
-from ray.tune import register_env
-from ray import tune, train
-from ray.air import CheckpointConfig
-from ray.tune.schedulers import AsyncHyperBandScheduler
-from ray.rllib.algorithms.ppo import PPOConfig
+
+num_gpus = int(torch.cuda.is_available())
 
 ray.shutdown()
-ray.init()
+ray.init(num_gpus=num_gpus)
 
 IMPORT_PENALTY = 1  # 100
 EXPORT_PENALTY = 1  # 10
@@ -275,43 +260,38 @@ algo = None
 current_best = None
 
 # Create the environment to train on
-env = EnergyCommunityContributionPriorityV5(ren_generators=generators,
-                                   generators=[],
-                                   loads=loads,
-                                   storages=storages,
-                                   evs=evs,
-                                   aggregator=aggregator,
-                                   storage_penalty=STORAGE_ACTION_PENALTY,
-                                   ev_penalty=EV_REQUIREMENT_PENALTY,
-                                   balance_penalty=BALANCE_PENALTY,
-                                   look_ahead=12,
-                                   max_episode_length=N_STEPS,
-                                   seed=42)
-register_env("EC_Contrib_V5", lambda config: env)
+env = EnergyCommunityContributionPriorityV6(ren_generators=generators,
+                                            generators=[],
+                                            loads=loads,
+                                            storages=storages,
+                                            evs=evs,
+                                            aggregator=aggregator,
+                                            storage_penalty=STORAGE_ACTION_PENALTY,
+                                            ev_penalty=EV_REQUIREMENT_PENALTY,
+                                            balance_penalty=BALANCE_PENALTY,
+                                            look_ahead=12,
+                                            max_episode_length=N_STEPS - 1,
+                                            seed=42,
+                                            is_training=True)
+register_env("EC_Contrib_V6", lambda config: env)
 
 # Define the PPOConfig
 _config = (PPOConfig()
-           .environment(env="EC_Contrib_V5", disable_env_checking=False)
-           .training(train_batch_size=128,
-                     lr=5e-5,
-                     gamma=0.99,
-                     use_gae=True,
-                     use_critic=True,
-                     use_kl_loss=True,
-                     clip_param=0.1,
-                     grad_clip=4,
-                     )
+           .environment(env="EC_Contrib_V6", disable_env_checking=False)
+           .training(train_batch_size=12288,
+                     sgd_minibatch_size=2048,
+                     num_sgd_iter=5,
+                     lr=1e-4,
+                     gamma=0.99)
            .exploration(exploration_config={})
            .framework('torch')
+           .resources(num_cpus_per_worker=num_gpus)
            .multi_agent(policies=policies,
-                        policies_to_train=list(policies.keys())[:-1],
-                        policy_mapping_fn=(lambda agent_id, episode, worker, **kwargs:
-                                           'storage' if agent_id.startswith('storage') else
-                                           agent_id if agent_id.startswith('ev') else 'aggregator'),
-                        algorithm_config_overrides_per_module=model_cfgs)
-           .rollouts(batch_mode='complete_episodes',
-                     num_rollout_workers=1,
-                     rollout_fragment_length=128))
+                        policy_mapping_fn=(lambda agent_id, episode, worker, **kwargs: agent_id))
+           .rollouts(batch_mode='truncate_episodes',
+                     num_rollout_workers=6,
+                     num_envs_per_worker=2,
+                     rollout_fragment_length=1024))
 
 # Clear the Jupyter cell output
 IPython.core.display_functions.clear_output()
@@ -326,7 +306,7 @@ scheduler = AsyncHyperBandScheduler(time_attr="training_iteration",
 tuner = tune.Tuner(
     "PPO",
     param_space=_config,
-    run_config=train.RunConfig(stop={'training_iteration': MAX_ITER, 'episode_reward_mean': -40.0},
+    run_config=train.RunConfig(stop={'training_iteration': MAX_ITER},
                                checkpoint_config=CheckpointConfig(checkpoint_frequency=100,
                                                                   checkpoint_at_end=True),
                                verbose=1),
